cdf9a35bb60a2e5422aad460c5a2dc81548a46cd
yukkuOO <87624435+yukkuOO@users.noreply.github.com>
2021-08-05 14:50:29 +0900

    Add files via upload

--- main/pf_main.js
+++ main/pf_main.js
@@ -2,7 +2,7 @@ $(function(){
 
 
     $("#main").fullpage({
-        anchors:['xm01','xm02','xm03','xm04','xm05','xm06','xm07'],
+        anchors:['xm01','xm02','xm03','xm04','xm05','xm06','xm07','xm08'],
         afterLoad: function(origin,destination, direction){
             $(".section").eq(destination.index).addClass("on").siblings().removeClass("on");
             $("nav li").eq(destination.index).addClass("on").siblings().removeClass("on");
@@ -30,7 +30,30 @@ $(function(){
     });
     
     
+    $(".artworkli").slick({
+        arrows:false,
+        dots:true,
+        slidesToShow: 3,
+        
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+                
+    
+              }
+            },
+        ]
+    });
 
+
+    $(".pf07 .arr .xi-angle-left-min").on("click",function(){
+        $(".artworkli").slick("slickPrev");
+    });
+    $(".pf07 .arr .xi-angle-right-min").on("click",function(){
+        $(".artworkli").slick("slickNext");
+    });
 
 
 })
